9e491e43a787ebf4434149c647684d9d6f96b1c5
Pete Snyder <cybernexus00@yahoo.com>
2025-02-05 13:12:18 -0500

    [MIG] product_partner_restriction: Migration to 18.0

--- product_partner_restriction/__manifest__.py
+++ product_partner_restriction/__manifest__.py
@@ -4,7 +4,7 @@
     "sequence": 55,
     "summary": "Adds ability to define partner specific products",
     "website": "https://github.com/OCA/product-attribute",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "OPL-1",
     "author": "Cybernexus Solutions",
     "depends": ["sale"],
