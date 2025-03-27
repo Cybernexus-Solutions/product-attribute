e56a866c1f4dec0c1cfe37b921654749a989a917
Pete Snyder <cybernexus00@yahoo.com>
2025-03-27 13:19:18 -0400

    [FIX] product_dimension: volume calculations should take the config param setting into account for volume instead of staticly converting to meters

--- product_dimension/models/product_template.py
+++ product_dimension/models/product_template.py
@@ -36,10 +36,10 @@ class ProductTemplate(models.Model):
     def _calc_volume(self, product_length, product_height, product_width, uom_id):
         volume = 0
         if product_length and product_height and product_width and uom_id:
-            length_m = self.convert_to_meters(product_length, uom_id)
-            height_m = self.convert_to_meters(product_height, uom_id)
-            width_m = self.convert_to_meters(product_width, uom_id)
-            volume = length_m * height_m * width_m
+            length_unit = self.convert_to_unit(product_length, uom_id)
+            height_unit = self.convert_to_unit(product_height, uom_id)
+            width_unit = self.convert_to_unit(product_width, uom_id)
+            volume = length_unit * height_unit * width_unit
 
         return volume
 
@@ -55,12 +55,13 @@ class ProductTemplate(models.Model):
                 template.dimensional_uom_id,
             )
 
-    def convert_to_meters(self, measure, dimensional_uom):
-        uom_meters = self.env.ref("uom.product_uom_meter")
+    def convert_to_unit(self, measure, dimensional_uom):
+        product_length_in_feet = self.env["ir.config_parameter"].sudo().get_param("product.volume_in_cubic_feet")
+        uom_unit = self.env.ref("uom.product_uom_foot") if product_length_in_feet else self.env.ref("uom.product_uom_meter") 
 
         return dimensional_uom._compute_quantity(
             qty=measure,
-            to_unit=uom_meters,
+            to_unit=uom_unit,
             round=False,
         )
 
